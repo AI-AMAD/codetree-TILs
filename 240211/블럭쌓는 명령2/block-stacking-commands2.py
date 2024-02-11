a3f6bea1175d996fadec1e5e39d84c95b0f26499
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-02-11 18:17:17 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240211/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+block_numbers = [0 for _ in range(n+1)]
+
+for i in range(k):
+    a, b = input().split()
+    for j in range(a, b+1):
+        block_numbers[j] += 1
+print(max(block_numbers))
