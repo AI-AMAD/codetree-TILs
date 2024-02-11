ec4004a6528598014e8ecf1b47dce6ee3e291d7a
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-02-11 14:07:52 +0900

    Title: 오름 내림차순 정렬, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240211/오름 내림차순 정렬/inc-dec-sorting.py
+++ 240211/오름 내림차순 정렬/inc-dec-sorting.py
@@ -2,5 +2,11 @@ n = int(input())
 numbers = [i for i in map(int, input().split())]
 numbers = sorted(numbers)
 numberst = numbers[::-1]
-print(numbers)
-print(numberst)
+
+for i in numbers:
+    print(i, end=' ')
+
+print()
+
+for j in numberst:
+    print(j, end=' ')
