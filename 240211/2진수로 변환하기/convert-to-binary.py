e2fd777d71a3f7ac3f34cbb6e9918ff8f11930b3
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-02-11 17:59:11 +0900

    Title: 2진수로 변환하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240211/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,13 @@
+n = int(input())
+result = []
+
+def recur(n):
+    if n == 1:
+        result.append(n)
+        return
+    else:
+        result.append(n%2)
+        recur(n//2)
+recur(n)
+for i in result[::-1]:
+  print(i,end='')
