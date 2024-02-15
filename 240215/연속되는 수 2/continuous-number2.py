b6a8764372cc7e81093e27bb75ae6b6d9d1746cc
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-02-15 17:47:13 +0900

    Title: 연속되는 수 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240215/연속되는 수 2/continuous-number2.py
+++ 240215/연속되는 수 2/continuous-number2.py
@@ -7,6 +7,7 @@ temp = 0
 for i in range(n):
   if i == 0 or numbers[i] == numbers[i-1]:
     temp += 1
+    max_continuity = max(max_continuity, temp)
   elif numbers[i] != numbers[i-1]:
     max_continuity = max(max_continuity, temp)
     temp = 1
