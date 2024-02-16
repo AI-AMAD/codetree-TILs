ea9bf8db3833334abb7d64c62b705351ff836f21
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-02-16 19:29:15 +0900

    Title: 선두를 지켜라, Time: 223ms, Memory: 50MB, Status: Passed - CodeTree

---
+++ 240216/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,48 @@
+n, m = map(int, input().split())
+information = [
+  tuple(map(int, input().split()))
+  for _ in range(n+m)
+]
+
+a_arr = []
+b_arr = []
+
+winner = None
+temp = None
+a_current = 0
+b_current = 0
+result = 0
+
+def winner_changed_check(a, b):
+  global winner
+  global temp
+  if a > b:
+    temp = "a"
+    if winner == temp:
+      return False
+    else:
+      winner = temp
+      return True
+  else:
+    temp = "b"
+    if winner == temp:
+      return False
+    else:
+      winner = temp
+      return True
+
+for i, (speed, time) in enumerate(information):
+  if i < n:
+    for _ in range(time):
+      a_current += speed
+      a_arr.append(a_current)
+  else:
+    for _ in range(time):
+      b_current += speed
+      b_arr.append(b_current)
+
+for a, b in zip(a_arr, b_arr):
+  if winner_changed_check(a, b):
+    result += 1
+
+print(result-1)
