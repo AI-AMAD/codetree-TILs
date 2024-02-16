4c03f17b1ffe5ceacd18738c07b7df62a351db2b
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-02-16 14:53:06 +0900

    Title: 만나는 그 순간, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240216/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+
+move = [
+  tuple(input().split())
+  for _ in range(n+m)
+]
+
+a_current = 0
+b_current = 0
+
+a_arr = [0]
+b_arr = [0]
+
+for i, (direction, move_count) in enumerate(move, start=1):
+  if i <= n:
+    move_count = int(move_count)
+    for count in range(1, move_count+1):
+      if direction == "R":
+        a_arr.append(a_current+count)
+      else:
+        a_arr.append(a_current-count)
+    a_current = a_arr[-1]
+
+  elif i >= m:
+    move_count = int(move_count)
+    for count in range(1, move_count+1):
+      if direction == "R":
+        b_arr.append(b_current+count)
+      else:
+        b_arr.append(b_current-count)
+    b_current = b_arr[-1]
+
+result = -1
+
+for i, (a, b) in enumerate(zip(a_arr, b_arr)):
+  if i != 0 and a == b:
+    result = i
+    break
+print(result)
