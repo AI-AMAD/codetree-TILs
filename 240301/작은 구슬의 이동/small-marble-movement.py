0310d75f78eb28cb35cb4c86dc8a9d4288257a21
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-03-01 19:42:59 +0900

    Title: 작은 구슬의 이동, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,46 @@
+n, time = map(int, input().split())
+y, x, direction = input().split()
+
+y = int(y)-1
+x = int(x)-1
+print(direction)
+board = [ 
+  [0]*n
+  for _ in range(n)
+]
+
+dys = [0, -1, 1, 0]
+dxs = [1, 0, 0, -1]
+
+direction_map = {
+  "R": 0,
+  "D": 1,
+  "U": 2,
+  "L": 3
+}
+
+reverse_direction_map = {
+  "0": "R",
+  "1": "D",
+  "2": "U",
+  "3": "L"
+}
+
+def check(y, x):
+  return y >= 0 and x >= 0 and y < n and x < n
+
+while True:
+  if time == 0:
+    break
+  ny = y + dys[direction_map[direction]]
+  nx = x + dxs[direction_map[direction]]
+  if check(ny, nx):
+    time -= 1
+    y = ny
+    x = nx
+  else:
+    time -= 1
+    direction = 3 - direction_map[direction]
+    direction = reverse_direction_map[str(direction)]
+
+print(y+1, x+1)
