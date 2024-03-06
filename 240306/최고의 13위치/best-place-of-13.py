7197a6456c5ff9bab8660ef8709dcc24d6a6fa82
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-03-06 14:52:04 +0900

    Title: 최고의 13위치, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240306/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,17 @@
+n = int(input())
+board = [
+  list(map(int, input().split()))
+  for _ in range(n)
+]
+
+result = 0
+def gold_check(y, x):
+  gold = board[y][x]+board[y][x+1]+board[y][x+2]
+  return gold
+
+for y in range(n-2):
+  for x in range(n-2):
+    temp = gold_check(y, x)
+    result = max(result, temp)
+
+print(result)
