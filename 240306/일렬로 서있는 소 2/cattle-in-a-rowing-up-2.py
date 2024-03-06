617913ec03f0593b7b5f6775ab88991e634d23c8
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-03-06 14:30:55 +0900

    Title: 일렬로 서있는 소 2, Time: 184ms, Memory: 25MB, Status: Passed - CodeTree

--- 240306/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240306/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ result = 0
 for i in range(n):
   for j in range(i+1, n):
     for k in range(j+1, n):
-      if i<j<k and cow_list[i]<cow_list[j]<cow_list[k]:
+      if i<j<k and cow_list[i]<=cow_list[j]<=cow_list[k]:
         result += 1
 
 print(result)
