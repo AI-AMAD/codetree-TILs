49d428d09f3cc1d8b0e3251722e86ac18e67c1e9
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-03-06 14:53:09 +0900

    Title: 최고의 13위치, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/최고의 13위치/best-place-of-13.py
+++ 240306/최고의 13위치/best-place-of-13.py
@@ -9,7 +9,7 @@ def gold_check(y, x):
   gold = board[y][x]+board[y][x+1]+board[y][x+2]
   return gold
 
-for y in range(n-2):
+for y in range(n):
   for x in range(n-2):
     temp = gold_check(y, x)
     result = max(result, temp)
