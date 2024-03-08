99e9ab50bc2ae00260ac4bee58e4df0c87335681
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-03-08 11:00:50 +0900

    Title: 이상한 진수 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240308/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,22 @@
+target = list(input())
+end = len(target)
+result = 0
+
+def change_to_decimal(number_list):
+  binary_number_list = ['0','b']
+  binary_number_list.extend(number_list)
+  binary_number = ''.join(binary_number_list)
+  decimal = int(binary_number, 2)
+  return decimal
+
+for i in range(1, end):
+  if target[i] == '0':
+    target[i] = '1'
+    temp = change_to_decimal(target)
+    result = max(temp, result)
+  elif target[i] == '1':
+    target[i] = '0'
+    temp = change_to_decimal(target)
+    result = max(temp, result)
+
+print(result)
