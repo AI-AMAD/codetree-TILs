b0175f5e3a3db9cc36d4cebe72df3ffabcc11b2f
jungchan-Lim <91253866+AI-AMAD@users.noreply.github.com>
2024-03-16 15:55:40 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240316/오목/O-mok.py
+++ 240316/오목/O-mok.py
@@ -6,21 +6,48 @@ board = [
 result = []
 flag = False
 
+def in_range_garo(i, j):
+  return i<15
+
+def in_range_sero_and_dialogue_up(i, j):
+  return j<15
+
+def in_range_dialogue_down(i, j):
+  return i<15 and j<15
+
 def garo_check(i, j):
-  if board[i][j] == board[i][j+1] == board[i][j+2] == board[i][j+3] == board[i][j+4]:
-    return True
+  if in_range_garo(i, j):
+    if board[i][j] == board[i][j+1] == board[i][j+2] == board[i][j+3] == board[i][j+4]:
+      return True
+    else:
+      return False
   else:
     return False
 
 def sero_check(i, j):
-  if board[i][j] == board[i+1][j] == board[i+2][j] == board[i+3][j] == board[i+4][j]:
-    return True
+  if in_range_sero_and_dialogue_up(i, j):
+    if board[i][j] == board[i+1][j] == board[i+2][j] == board[i+3][j] == board[i+4][j]:
+      return True
+    else:
+      return False
+  else: 
+    return False
+
+def dialogue_check_down(i, j):
+  if in_range_dialogue_down(i, j):
+    if board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3] == board[i+4][j+4]:
+      return True
+    else:
+      return False
   else:
     return False
 
-def dialogue_check(i, j):
-  if board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3] == board[i+4][j+4]:
-    return True
+def dialogue_check_up(i, j):
+  if in_range_sero_and_dialogue_up(i, j):
+    if board[i][j] == board[i-1][j+1] == board[i-2][j+2] == board[i-3][j+3] == board[i-4][j+4]:
+      return True
+    else:
+      return False
   else:
     return False
 
@@ -36,9 +63,14 @@ def check(i, j):
       result.append(i+3)
       result.append(j+1)
       return result
-    elif dialogue_check(i, j):
+    elif dialogue_check_down(i, j):
       result.append(board[i][j])
       result.append(i+3)
+      result.append(j+3)
+      return result
+    elif dialogue_check_up(i, j):
+      result.append(board[i][j])
+      result.append(i-1)
       result.append(j+3)
       return result
   elif board[i][j] == 2:
@@ -52,18 +84,23 @@ def check(i, j):
       result.append(i+3)
       result.append(j+1)
       return result
-    elif dialogue_check(i, j):
+    elif dialogue_check_down(i, j):
       result.append(board[i][j])
       result.append(i+3)
+      result.append(j+3)
+      return result
+    elif dialogue_check_up(i, j):
+      result.append(board[i][j])
+      result.append(i-1)
       result.append(j+3)
       return result
 
   return result
 
-for i in range(15):
+for i in range(19):
   if flag == True:
     break
-  for j in range(15):
+  for j in range(19):
     number = board[i][j]
     if number == 0:
       continue
